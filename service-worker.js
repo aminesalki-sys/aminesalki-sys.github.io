12e37670a3b810e795f02157f2fac91a63faddb8
aminesalki-sys <amine.salki@gmail.com>
2026-07-14 15:43:56 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,1 @@
-const CACHE_NAME = 'ejws-ux-portfolio-v2';
-self.addEventListener('install', event => { self.skipWaiting(); });
-self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))); self.clients.claim(); });
-self.addEventListener('fetch', event => { return; });
+self.addEventListener('install',()=>self.skipWaiting());self.addEventListener('activate',e=>e.waitUntil((async()=>{for(const k of await caches.keys())await caches.delete(k);await self.registration.unregister();const clientsList=await self.clients.matchAll({type:'window'});for(const c of clientsList)c.navigate(c.url)})()));
