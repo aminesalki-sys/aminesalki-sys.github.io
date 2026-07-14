3d515f2f403660592186691c3f7e255592b1224c
aminesalki-sys <amine.salki@gmail.com>
2026-07-14 14:42:36 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ejws-editorial-v20260714';
+const CACHE_NAME = 'ejws-ux-portfolio-v2';
 self.addEventListener('install', event => { self.skipWaiting(); });
 self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))); self.clients.claim(); });
 self.addEventListener('fetch', event => { return; });
